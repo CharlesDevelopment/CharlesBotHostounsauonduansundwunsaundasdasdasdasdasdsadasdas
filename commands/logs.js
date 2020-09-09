097f54605a9d8dbf63c033ad00c710e989d62cf1
UwUDevelopment <70700642+UwUDevelopment@users.noreply.github.com>
2020-09-09 22:32:27 +0700

    Update logs.js

--- commands/logs.js
+++ commands/logs.js
@@ -7,9 +7,10 @@ module.exports.run = async (bot, message, args) => {
         .setThumbnail('https://cdn.discordapp.com/attachments/741546722400010281/752138723839442954/uwuken.jpg')
         .setFooter('+ Means Added , = Means Updated And - Means Deleted')
         .addFields(
-            { name: 'Update 06/09/2020', value: '`+ Added /help`   `+ Added /Memberinfo`   `+ Added /clear`', inline: false},
-            { name: 'Update 07/09/2020', value: '`+ Added /logs`   `+ Changes All Command With Photos Charles!`', inline: false},
-            { name: 'Update 08/09/2020', value: '`+ Added /meme`   `+ Added /cat`  `+ Added /dog`  `= Updated /help`   `+ Added /ping`', inline: false},
+            { name: 'Update 06/09/2020', value: '`+ Added /help`   `+ Added /Memberinfo`   `+ Added /clear`', inline: true},
+            { name: 'Update 07/09/2020', value: '`+ Added /logs`   `+ Changes All Command With Photos Charles!`', inline: true},
+            { name: 'Update 08/09/2020', value: '`+ Added /meme`   `+ Added /cat`  `+ Added /dog`  `= Updated /help`   `+ Added /ping`', inline: true},
+            { name: 'Update 09/09/2020', value: '`+ Added /serverinfo` `= Updated /clear`  `= Updated /help`', inline: true},
         )
         .setTimestamp()
 
