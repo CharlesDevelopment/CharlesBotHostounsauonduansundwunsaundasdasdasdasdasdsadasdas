8ab05b44e7484364e0cdd1888307503413fe78f0
UwUDevelopment <70700642+UwUDevelopment@users.noreply.github.com>
2020-09-10 11:54:17 +0700

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,10 +4,9 @@ module.exports.run = async (bot, message, args) => {
         const embed = new Discord.MessageEmbed()
  
         if(!message.mentions.users.first()){
-            const user = message.mentions.users.first()
             embed.setAuthor(member.user.tag, member.user.displayAvatarURL())
             embed.setTitle(`${user.tag}'s Avatar:`)
-            embed.setThumbnail(user.displayAvatarURL())
+            embed.setImage(user.displayAvatarURL())
             embed.setDescription(`This is ${user.tag}'s avatar.`)
             embed.setColor('RANDOM')
             embed.setFooter("")
