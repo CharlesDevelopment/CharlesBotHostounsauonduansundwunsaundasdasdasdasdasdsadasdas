6cf3742f25a45222dd863fd49543a29d2494864f
UwUDevelopment <70700642+UwUDevelopment@users.noreply.github.com>
2020-09-11 20:00:08 +0700

    Update F.js

--- commands/F.js
+++ commands/F.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js")
 module.exports.run = async (bot, message, args) => {
    let MSG = message.content.split(`/f `).join("")
    if(!MSG)return message.channel.send(`You Did Not Specify Your Message To Send!`)
-   message.channel.send(Press F For MSG)
+   message.channel.send(MSG)
    await MSG.react('🇫')
 }
 
