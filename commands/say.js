37a11a729f3356739eda237c42c458255c85e356
UwUDevelopment <70700642+UwUDevelopment@users.noreply.github.com>
2020-09-11 19:37:41 +0700

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,7 +1,10 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
-    message.channel.send(args.join(" "), { allowedMentions: { parse: ["user"]}});
+   let MSG = message.content.split(`/say `).join("")
+   if(!MSG)return message.channel.send(`You Did Not Specify Your Message To Send!`)
+   message.channel.send(MSG)
+   message.delete()
 }
 
 module.exports.config = {
