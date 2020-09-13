a8bd20df4ef6c20202eeecffc4eea008020b9053
UwUDevelopment <70700642+UwUDevelopment@users.noreply.github.com>
2020-09-13 12:05:29 +0700

    Create calcgay.js

---
+++ commands/calcgay.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+    if(!args[0]) return msg.reply("You didn't specify args 1")
+    if(!args[1]) return msg.reply("You didn't specify args 2")
+    msg.channel.send("Your random number is: " + Math.floor(Math.random() * args[1] + args[0]));
+};
+
+module.exports.config = {
+    name: "number",
+    description: "pUp :u",
+    usage: "/number",
+    accessableby: "Members",
+    aliases: []
+}
