eb4f03b238ed079998e009a3b0a16e5fe67071b1
UwUDevelopment <70700642+UwUDevelopment@users.noreply.github.com>
2020-09-13 12:24:43 +0700

    Update calcgay.js

--- commands/calcgay.js
+++ commands/calcgay.js
@@ -2,9 +2,7 @@ const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
     let msg = message;
-    if(!args[0]) return msg.reply("You didn't specify args 1")
-    if(!args[1]) return msg.reply("You didn't specify args 2")
-    msg.channel.send("Your random number is: " + Math.floor(Math.random() * args[1] + args[0]));
+    msg.channel.send("Your random number is: " + Math.floor(Math.random()
 };
 
 module.exports.config = {
