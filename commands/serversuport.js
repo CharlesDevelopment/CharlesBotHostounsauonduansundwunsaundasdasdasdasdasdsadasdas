1f1839df98aa62916beb8cb76625cfb48e04c542
UwUDevelopment <70700642+UwUDevelopment@users.noreply.github.com>
2020-09-16 13:41:49 +0700

    Create serversuport.js

---
+++ commands/serversuport.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+    const embed = new Discord.MessageEmbed()
+        .setTitle('Join Support Server !')
+        .setURL('https://discord.io/Charles')
+        .setAuthor('Charles Community')
+        .setDescription('⬆️ Click To Join Support Server ')
+        .setColor('RANDOM')
+        .setThumbnail('https://cdn.discordapp.com/attachments/741546722400010281/752138723839442954/uwuken.jpg')
+        .setFooter('If You Need Help Just join To The Server Support')
+        .setTimestamp()
+
+
+    message.channel.send(embed);
+}
+
+module.exports.config = {
+    name: "support",
+    description: "pUp :u",
+    usage: "/support",
+    accessableby: "Members",
+    aliases: []
+}
