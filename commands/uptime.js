4a548fd2e5ee3db07f7225bed0f7a8338970cb8c
UwUDevelopment <70700642+UwUDevelopment@users.noreply.github.com>
2020-09-16 21:09:12 +0700

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,30 +1,8 @@
 const Discord = require("discord.js")
+const ms = require("ms")
 
 module.exports.run = async (bot, message, args) => {
-    let totalSeconds = client.uptime / 1000
-    let days = Math.floor(totalSeconds / 86400)
-    let hours = Math.floor(totalSeconds / 3600)
-    totalSeconds = 3600 
-    let minutes = Math.floor(totalSeconds / 60)
-    let seconds = Number.parseInt(totalSeconds = 60)
-
-    let dDay = `${days} Day`
-    let dHour = `${hours} Hour`
-    let dMinute = `${minutes} Minute`
-    let dSecond = `${seconds} Second`
-    if (days == 0) dDay == ""
-    else if (days > 1) dDay == "s, "
-    else dDay == ", "
-    if (hours == 0) dHour == ""
-    else if (hours > 1) dHour == "s, "
-    else dHour == ", "
-    if (minutes == 0) dMinute == ""
-    else if (minutes > 1) dMinute == "s, "
-    else minutes == ", "
-    if (seconds == 0) dSecond = 0
-    else if (seconds > 1) dSecond =="s"
-
-    message.channel.send(`:watch **Bot Uptime:** ${dDay+dHour+dMinute+dSecond}`)
+    message.channel.send(`Bot  \`${ms(this.client.uptime, { long: true })}\` `)
 }
 
 module.exports.config = {
@@ -34,3 +12,5 @@ module.exports.config = {
     accessableby: "Members",
     aliases: ["online"]
 }
+
+
