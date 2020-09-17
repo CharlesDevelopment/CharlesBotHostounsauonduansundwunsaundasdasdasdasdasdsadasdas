de3426ff7b51c1480664cdfe82da5e049ac823dc
UwUDevelopment <70700642+UwUDevelopment@users.noreply.github.com>
2020-09-17 22:12:11 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 module.exports.run = async (bot, message, args) => {
 
         const ping = new Discord.MessageEmbed()
-        .setTitle(Pong!)
+        .setTitle('Pong!')
         .setDescription(`🏓\`${Date.now() - message.createdTimestamp}\`ms`);
 
 
