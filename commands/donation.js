435b5fa4db11f5ba2569f9f69283a756741a8b96
UwUDevelopment <70700642+UwUDevelopment@users.noreply.github.com>
2020-09-28 21:51:24 +0700

    gg

---
+++ commands/donation.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+    const embed = new Discord.MessageEmbed()
+        .setAuthor('Support Developer!')
+        .setDescription('Saweria')
+        .setURL('https://bit.ly/2GlCcoY')
+        .setDescription('Patreon')
+        .setURL('https://bit.ly/30gIXzD')
+        .setDescription('Support Server')
+        .setURL('https://bit.ly/3n0hhcc')
+        .setColor('RANDOM')
+        .setThumbnail('https://cdn.discordapp.com/attachments/741546722400010281/752138723839442954/uwuken.jpg')
+
+        .setTimestamp()
+
+
+    message.channel.send(embed);
+}
+
+module.exports.config = {
+    name: "invite",
+    description: "pUp :u",
+    usage: "/donate",
+    accessableby: "Members",
+    aliases: []
+}
