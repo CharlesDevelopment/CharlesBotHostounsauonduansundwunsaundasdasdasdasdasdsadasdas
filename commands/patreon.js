d018c45f71e815eb97cf6e663b8858d637a87cdc
UwUDevelopment <70700642+UwUDevelopment@users.noreply.github.com>
2020-09-29 19:28:12 +0700

    patreon

---
+++ commands/patreon.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    const embed = new Discord.MessageEmbed()
+        .setTitle('Donate !')
+        .setColor('RANDOM')
+        .setThumbnail('https://cdn.discordapp.com/attachments/741546722400010281/752138723839442954/uwuken.jpg')
+        .setDescription('Patreon')
+        .setURL('https://bit.ly/30gIXzD')
+        .setTimestamp()
+
+
+    message.channel.send(embed);
+}
+
+module.exports.config = {
+    name: "donate",
+    description: "example of an Embed.",
+    usage: "/donate patreon",
+    accessableby: "Members",
+    aliases: []
+}
