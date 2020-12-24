95d7f4a50518f79cfd400c04416a08d4b79bba6e
UwUDevelopment <70700642+UwUDevelopment@users.noreply.github.com>
2020-12-24 22:37:51 +0700

    Update logs.js

--- commands/logs.js
+++ commands/logs.js
@@ -7,11 +7,7 @@ module.exports.run = async (bot, message, args) => {
         .setThumbnail('https://cdn.discordapp.com/attachments/741546722400010281/752138723839442954/uwuken.jpg')
         .setFooter('+ Means Added , = Means Updated And - Means Deleted')
         .addFields(
-            { name: 'Update 07/09/2020', value: '`+ Added /logs`   `+ Changes All Command With Photos Charles!`', inline: true},
-            { name: 'Update 08/09/2020', value: '`+ Added /meme`   `+ Added /cat`  `+ Added /dog`  `= Updated /help`   `+ Added /ping`', inline: true},
-            { name: 'Update 09/09/2020', value: '`+ Added /serverinfo` `= Updated /clear`  `= Updated /help`', inline: true},
-            { name: 'Update 10/09/2020', value: '`+ Added /support` `+ Added Support Server`  `= Updated /help`', inline: true},
-            { name: 'Update 21/09/2020', value: '`= Updated /help`   `+ Added /ban`   `+ Added /kick`' , inline: true},
+            { name: 'Update 07/09/2020', value: '`+ Added /slowmode command`   `+Reset Logs`  `+ Added /rps (still Bug)`   `+ Added /nuke command', inline: true},
         )
         .setTimestamp()
 
