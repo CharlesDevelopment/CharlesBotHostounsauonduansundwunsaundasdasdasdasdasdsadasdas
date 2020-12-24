3e1670c2d22bd9dabacf439ace23198090949352
UwUDevelopment <70700642+UwUDevelopment@users.noreply.github.com>
2020-12-24 22:13:33 +0700

    update! (7)

---
+++ commands/slowmode.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+        if(!message.member.hasPermission("ADMINISTRATOR")) {
+            return message.reply("You don't have enough perms to use this command!")
+        }
+        let duration = args[0]
+        if(isNaN(duration)) return message.reply("Please give the time in seconds.")
+        let reason = args.slice(1).join(" ")
+        
+        message.channel.setRateLimitPerUser(duration, reason)
+        message.reply(`Successfully set the slowmode to ${duration}`)
+    }
+
+module.exports.config = {
+    name: "slowmode",
+    description: "Shows the information of a member/user",
+    usage: "/slowmode",
+    accessableby: "Admins",
+    aliases: []
+}
