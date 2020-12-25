e579650643ed989fb0337ef34f57d6df9f2e1a5b
UwUDevelopment <70700642+UwUDevelopment@users.noreply.github.com>
2020-12-25 17:36:00 +0700

    p

---
+++ commands/support.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+    const embed = new Discord.MessageEmbed()
+        .setTitle('Support')
+        .setAuthor('Contact AkuRahasia#3857 To Support!')
+        .setColor('RANDOM')
+        .setThumbnail('https://cdn.discordapp.com/attachments/741546722400010281/752138723839442954/uwuken.jpg')
+        .setTimestamp()
+        
+
+
+    message.channel.send(embed);
+}
+
+module.exports.config = {
+    name: "support",
+    description: "",
+    usage: "/support",
+    accessableby: "Members",
+    aliases: []
+}
