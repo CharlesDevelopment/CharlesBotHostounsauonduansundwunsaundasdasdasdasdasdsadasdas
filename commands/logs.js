93ef0fdf71311de5a613a8f31ac7116ff779f3cb
UwUDevelopment <70700642+UwUDevelopment@users.noreply.github.com>
2020-12-28 10:51:24 +0700

    Update logs.js

--- commands/logs.js
+++ commands/logs.js
@@ -4,6 +4,7 @@ module.exports.run = async (bot, message, args) => {
     const embed = new Discord.MessageEmbed()
         .setTitle('Status Bot')
         .setURL('https://bit.ly/Statusbot')
+        .setFooter('⬆️ Click To Check Status')
         .setColor('RANDOM')
         .setThumbnail('https://cdn.discordapp.com/attachments/741546722400010281/752138723839442954/uwuken.jpg')
         .setTimestamp()
