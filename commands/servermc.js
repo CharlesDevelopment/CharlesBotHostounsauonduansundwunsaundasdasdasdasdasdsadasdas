dbe3ddb374454a9cb2f1232e22980018ae6c3840
UwUDevelopment <70700642+UwUDevelopment@users.noreply.github.com>
2020-12-29 21:57:34 +0700

    Update (14)

--- commands/servermc.js
+++ commands/servermc.js
@@ -1,34 +1,76 @@
-const Discord = require("discord.js")
-const ping = require("minecraft-server-util")
+const util = require('minecraft-server-util');
+const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
- 
-    switch(args[0]){
-        case 'mc':
- 
-            if(!args[1]) return message.channel.send('You must type a minecraft server ip')
-            if(!args[2]) return message.channel.send('You must type a minecraft server port')
- 
-            ping(args[1], parseInt(args[2]), (error, reponse) =>{
-                if(error) throw error
-                const Embed = new RichEmbed()
-                .setTitle('Server Status')
-                .addField('Server IP', reponse.host)
-                .addField('Server Version', reponse.version)
-                .addField('Online Players', reponse.onlinePlayers)
-                .addField('Max Players', reponse.maxPlayers)
-                
-                message.channel.send(Embed)
+    const server = {
+        ip: '0.0.0.0', // Put your minecraft server IP or hostname here (e.g. '192.168.0.1')
+        port: 25565 // Put your minecraft server port here (25565 is the default)
+    };
+    const commands = {
+        status: {
+            command: '.status',
+            text: {
+                error: 'Error getting Minecraft server status...', // Check your terminal when you see this
+                online: '**Minecraft** server is **online**  -  ',
+                players: '**{online}** people are playing!', // {online} will show player count
+                noPlayers: '**Nobody is playing**'
+            }
+            
+        },
+        ip: {
+            command: '.ip',
+            text: {
+                main: 'The IP for the server is `{ip}:{port}`' // {ip} and {port} will show server ip and port from above
+            }
+        }
+    };
+    
+    // Do not edit below this line unless you know what you're doing
+    
+    const cacheTime = 30 * 1000; // 30 sec cache time
+    let data, lastUpdated = 0;
+    
+    client.on('message', message => { // Listen for messages and trigger commands
+        if(message.content.trim() == commands.status.command) {
+            statusCommand(message)
+        } else if(message.content.trim() == commands.ip.command) {
+            ipCommand(message)
+        }
+    });
+    
+    function statusCommand(message) { // Handle status command
+        if(Date.now() > lastUpdated + cacheTime) { // Cache expired or doesn't exist
+            util.status(server.ip, { port: server.port })
+            .then(res => {
+                data = res;
+                lastUpdated = Date.now();
+                replyStatus(message)
             })
-        break
- 
+            .catch(err => {
+                console.error(err);
+                return message.reply(commands.status.text.error);
+            });
+        } else { // Use cached data
+            replyStatus(message)
+        }
     }
- 
+    
+    function replyStatus(message) {
+        let { text } = commands.status;
+        let status = text.online;
+        status += data.onlinePlayers ? text.players : text.noPlayers;
+        status = status.replace('{online}', data.onlinePlayers);
+        message.reply(status);
+    }
+    
+    function ipCommand(message) { // Handle IP command
+        message.reply(commands.ip.text.main.replace('{ip}', server.ip).replace('{port}', server.port));
+    }
 }
 
 module.exports.config = {
-    name: "mcserver",
-    description: "Shows the information of a member/user",
+    name: "servermc",
+    description: "Shows the information of a server",
     usage: "/servermc",
     accessableby: "Members",
     aliases: []
